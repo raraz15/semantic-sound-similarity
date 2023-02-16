31ea2e72a412cdcbdb44c72e913431637e42782f
raraz15 <oguza97@gmail.com>
2023-02-16 15:28:05 +0100

    frame aggregation method

--- embedding_analyzer.py
+++ embedding_analyzer.py
@@ -8,12 +8,32 @@ import numpy as np
 
 ANALYSIS_DIR = "analysis"
 
+# TODO: frame aggregation
+# TODO: energy based frame filtering
+# TODO: PCA
+def aggregate_frames(embeds, normalize=True, aggregation="none"):
+    """ Takes a list of embeddings and aggregates them into a clip level embedding.
+    """
+    # Convert to numpy array
+    if type(embeds)==list:
+        embeds = np.array(embeds)
+    # Normalize each time frame by itself if specified
+    if normalize:
+        embeds = embeds/np.linalg.norm(embeds,axis=1)[..., np.newaxis]
+    # Aggreagate
+    if aggregation=="mean":
+        embeds = embeds.mean(axis=0)
+    elif aggregation=="median":
+        embeds = np.median(embeds, axis=0)
+    return embeds
+
 # TODO: for large sound collections, write the output when a row is complete
 if __name__=="__main__":
 
     parser=argparse.ArgumentParser(description='Embedding analyzer.')
     parser.add_argument('-p', '--path', type=str, required=True, help='Path to directory containing embedding json files.')
-    parser.add_argument('-N', type=int, default=3, help="Number of queries to return.")
+    parser.add_argument("-a", "-aggregation", type=str, default="mean", help="Type of embedding aggregation.")
+    parser.add_argument('-N', type=int, default=10, help="Number of queries to return.")
     args=parser.parse_args()
 
     # Read all the json files in the tree
@@ -26,12 +46,16 @@ if __name__=="__main__":
         with open(embed_path, 'r') as infile: # Load the json file
             model_outputs = json.load(infile)
         if model_outputs['embeddings'] is not None: # Filter out the None types
+            # Aggregate frame level embeddings into a clip level embedding
+            clip_embedding = aggregate_frames(model_outputs["embeddings"], 
+                                              aggregation=args.a)
             embeddings.append({"audio_path": model_outputs["audio_path"], 
-                                "embeddings": model_outputs["embeddings"]})
+                                "embeddings": clip_embedding})
             if len(model_outputs["audio_path"]) > max_str_len: # For pretty print
                 max_str_len = len(model_outputs["audio_path"])
     print(f"{len(embeddings)} embeddings were read.")
 
+    # TODO: Nearest Neighbor, ANN
     # Compute pairwise dot products of normalized embeddings
     print("Computing pairwise dot products...")
     start_time = time.time()
