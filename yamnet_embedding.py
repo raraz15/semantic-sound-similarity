571ef9c2b600820d1c01e0b36925c58001a7658f
raraz15 <oguza97@gmail.com>
2023-02-16 11:11:51 +0100

    no aggregation possibility

--- yamnet_embedding.py
+++ yamnet_embedding.py
@@ -11,22 +11,31 @@ TRIM_DUR = 30
 SAMPLE_RATE = 16000
 ANALYZER_NAME = 'audioset-yamnet_v1'
 MODEL_PATH = "models/yamnet/audioset-yamnet-1.pb"
+#EVAL_CSV_PATH = "/data/FSD50K/FSD50K.ground_truth/eval.csv"
 EMBEDDINGS_DIR = f"embeddings/{ANALYZER_NAME}"
 AUDIO_DIR = "/data/FSD50K/FSD50K.eval_audio"
 
 # TODO: frame aggregation, frame filtering, PCA
 # TODO: only discard non-floatable frames?
-def get_clip_embedding(model, audio):
+# TODO: change name
+def get_clip_embedding(model, audio, aggregation="mean"):
     """ Takes an embedding model and an audio array and returns the clip level embedding.
     """
     try:
-        embedding = model(audio).mean(axis=0)  # Take mean of 1-second frame embeddings
-        embedding = [float(value) for value in embedding] # Needs to be a list of non-np types so that JSON can encode it
+        embeddings = model(audio) # Embedding vectors of each frame
+        if aggregation=="mean":
+            embedding = embeddings.mean(axis=0)  # Take mean of 1-second frame embeddings
+            embedding = [float(value) for value in embedding] # Needs to be a list of non-np types so that JSON can encode
+            return embedding
+        elif aggregation=="None":
+            embeddings = [[float(value) for value in embedding] for embedding in embeddings]
+            return embeddings
+        else:
+            raise NotImplementedError
     except AttributeError:
-        embedding = None
-    return embedding
+        return None
 
-def process_audio(model_embeddings, audio_path, output_dir):
+def process_audio(model_embeddings, audio_path, output_dir, aggregation="mean"):
 
     # Load the audio file
     loader = EasyLoader()
@@ -34,7 +43,7 @@ def process_audio(model_embeddings, audio_path, output_dir):
     audio = loader()
 
     # Process
-    embedding = get_clip_embedding(model_embeddings, audio)
+    embedding = get_clip_embedding(model_embeddings, audio, aggregation)
 
     # Save results
     fname = os.path.splitext(os.path.basename(audio_path))[0]
@@ -45,11 +54,14 @@ def process_audio(model_embeddings, audio_path, output_dir):
 if __name__=="__main__":
 
     parser=argparse.ArgumentParser(description='YAMNet Explorer.')
-    parser.add_argument('-p', '--path', type=str, required=True, help='Path to csv file containing audio paths.')
+    parser.add_argument('-p', '--path', type=str, required=True, help='Path to csv file containing fnames.')
+    parser.add_argument("-a", "-aggregation", type=str, default="mean", help="Type of embedding aggregation.")
     args=parser.parse_args()
 
     # Configure the embedding model
-    model_embeddings = TensorflowPredictVGGish(graphFilename=MODEL_PATH, input="melspectrogram", output="embeddings")
+    model_embeddings = TensorflowPredictVGGish(graphFilename=MODEL_PATH, 
+                                               input="melspectrogram", 
+                                               output="embeddings")
 
     # Read the file names
     fnames = pd.read_csv(args.path)["fname"].to_list()
@@ -58,15 +70,15 @@ if __name__=="__main__":
 
     # Create the output directory
     subset = os.path.splitext(os.path.basename(args.path))[0]
-    output_dir = os.path.join(EMBEDDINGS_DIR, subset)
+    output_dir = os.path.join(EMBEDDINGS_DIR, subset, args.a) # model_name/audio_set/aggregation
     os.makedirs(output_dir, exist_ok=True)
     print(f"Exporting the embeddings to: {output_dir}")
 
     # Process each audio
     start_time = time.time()
     for i,audio_path in enumerate(audio_paths):
-        print(f"[{i}/{len(audio_paths)}]")
-        process_audio(model_embeddings, audio_path, output_dir)
+        print(f"\n[{i}/{len(audio_paths)}]")
+        process_audio(model_embeddings, audio_path, output_dir, args.a)
     total_time = time.time()-start_time
     print(f"\nTotal time: {time.strftime('%H:%M:%S', time.gmtime(total_time))}")
     print(f"Average time/file: {total_time/len(audio_paths):.2f} sec.")
