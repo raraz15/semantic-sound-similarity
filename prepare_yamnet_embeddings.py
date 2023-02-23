ac365a27ea82f5ee9b4aed02b81a6e3f3b7d9de1
raraz15 <oguza97@gmail.com>
2023-02-23 23:02:07 +0100

    yamnet_embed prepare separate

---
+++ prepare_yamnet_embeddings.py
@@ -0,0 +1,126 @@
+import os
+import argparse
+import time
+import glob
+import yaml
+import json
+from pathlib import Path
+
+import numpy as np
+from sklearn.decomposition import PCA
+
+AUDIO_DIR = "/data/FSD50K/FSD50K.eval_audio"
+
+# In a preproccesing script,
+# TODO: energy based frame filtering (at audio input)
+# TODO: PCA
+
+def get_file_name(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+def aggregate_frames(embeds, normalize=True, aggregation="mean"):
+    """ Takes a list of embeddings and aggregates them into a 
+    clip level embedding, if not already aggregated."""
+    # Convert to numpy array
+    if type(embeds)==list:
+        embeds = np.array(embeds)
+    # Aggreagate if multiple frames exist and specified
+    if aggregation!="none" and len(embeds.shape)!=1:
+        if aggregation=="mean":
+            embeds = embeds.mean(axis=0)
+        elif aggregation=="median":
+            embeds = np.median(embeds, axis=0)
+        elif aggregation=="max":
+            embeds = embeds.max(axis=0)
+    # Normalize the clip level embedding
+    if normalize:
+        embeds = embeds/np.linalg.norm(embeds)
+    return embeds
+
+if __name__=="__main__":
+
+    parser=argparse.ArgumentParser(description=__doc__, 
+                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-p', '--path', type=str, required=True, 
+                        help='Directory containing embedding.json files.')
+    parser.add_argument("-a", "-aggregation", 
+                        type=str, 
+                        choices=["mean", "median", "max", "none"], 
+                        default="mean", 
+                        help="Type of embedding aggregation.")
+    parser.add_argument("--no-normalization",
+                        action="store_false", 
+                        help="Do not normalize the aggregated clip embedding.")
+    parser.add_argument("-N", type=int, default=100, 
+                        help="Number of PCA components to keep.")
+    parser.add_argument('--plot-scree', action='store_true', 
+                        help="Plot variance contributions of PCA components.")
+    args=parser.parse_args()
+
+    # Read all the json files in the tree
+    embed_paths = glob.glob(os.path.join(args.path, "**", "*.json"), recursive=True)
+    print(f"{len(embed_paths)} embeddings were found in the directory.")
+
+    # Load the embeddings and process them
+    print("Reading the embeddings and processing them...")
+    start_time = time.time()
+    embeddings = []
+    for embed_path in embed_paths:
+        with open(embed_path, 'r') as infile:
+            model_outputs = json.load(infile)
+        # Process and collect
+        if model_outputs['embeddings'] is not None: # Filter out the None types
+            clip_embedding = aggregate_frames(model_outputs["embeddings"], 
+                                              normalize=args.no_normalization, 
+                                              aggregation=args.a)
+            embeddings.append(clip_embedding)
+    embeddings = np.array(embeddings)
+    print(f"{len(embeddings)} embeddings were read.")
+    total_time = time.time()-start_time
+    print(f"Total pre-processing time: {time.strftime('%H:%M:%S', time.gmtime(total_time))}")
+
+    # Apply PCA
+    print("Applying PCA...")
+    start_time = time.time()
+    n_components = args.N if not args.plot_scree else None # Keep None for plotting scree
+    pca = PCA(n_components=n_components)
+    embeddings = pca.fit_transform(embeddings)
+    total_time = time.time()-start_time
+    print(f"Total time: {time.strftime('%H:%M:%S', time.gmtime(total_time))}")
+
+    # Create the output dir
+    output_dir = f"{args.path}-Agg_{args.a}-PCA_{args.N}"
+    os.makedirs(output_dir, exist_ok=True)
+    print(f"Exporting the embeddings to: {output_dir}...")
+
+    # Scree plot
+    if args.plot_scree:
+        print(f"Plotting the PCA Scree plot next to the embeddings...")
+        import matplotlib.pyplot as plt
+        model = os.path.basename(args.path)
+        data = os.path.basename(os.path.dirname(args.path))
+        title=f'FSD50K.{data} - {model} Embeddings PCA Scree Plot'
+        PC_values = np.arange(pca.n_components_) + 1
+        cumsum_variance = 100*np.cumsum(pca.explained_variance_ratio_)
+        fig,ax = plt.subplots(figsize=(15,8), constrained_layout=True)
+        fig.suptitle(title, fontsize=20)
+        ax.plot(PC_values, cumsum_variance, 'ro-', linewidth=2)
+        ax.set_xlim([-5,len(PC_values)+5])
+        ax.set_yticks(np.arange(0,105,5)) # 5% increase
+        ax.set_xlabel('Number of Principal Components Selected', fontsize=15)
+        ax.set_ylabel('% Cumulative Variance Explained', fontsize=15)
+        ax.grid()
+        figure_path = os.path.join(output_dir, f'FSD50K.{data}-{model}-scree_plot.jpeg')
+        fig.savefig(figure_path)
+
+    # Export the transformed embeddings
+    for embed_path,embed in zip(embed_paths,embeddings):
+        fname = get_file_name(embed_path)
+        embed = {"audio_path": os.path.join(AUDIO_DIR,f"{fname}.wav"),
+                "embeddings": embed.tolist()}
+        output_path = os.path.join(output_dir, f"{fname}.json")
+        with open(output_path, "w") as outfile:
+            json.dump(embed, outfile, indent=4)
+
+    #############
+    print("Done!")
--- similarity_search.py
+++ similarity_search.py
@@ -11,29 +11,6 @@ import numpy as np
 
 ANALYSIS_DIR = "data/similarity_results"
 
-# In a preproccesing script,
-# TODO: energy based frame filtering (at audio input)
-# TODO: PCA
-
-def aggregate_frames(embeds, normalize=True, aggregation="mean"):
-    """ Takes a list of embeddings and aggregates them into a 
-    clip level embedding, if not already aggregated."""
-    # Convert to numpy array
-    if type(embeds)==list:
-        embeds = np.array(embeds)
-    # Aggreagate if multiple frames exist and specified
-    if aggregation!="none" and len(embeds.shape)!=1:
-        if aggregation=="mean":
-            embeds = embeds.mean(axis=0)
-        elif aggregation=="median":
-            embeds = np.median(embeds, axis=0)
-        elif aggregation=="max":
-            embeds = embeds.max(axis=0)
-    # Normalize the clip level embedding
-    if normalize:
-        embeds = embeds/np.linalg.norm(embeds)
-    return embeds
-
 def dot_product_search(query, corpus, N):
     """Computes pairwise dot product similarities and returns the indices of top N"""
     assert len(query.shape)==1, f"To use dot product search, queries should be aggregated! {query.shape}"
@@ -57,60 +34,42 @@ def search_similar_sounds(query, corpus, N, algo="dot"):
         raise NotImplementedError
 
 # TODO: for large sound collections, write the output when a row is complete
-# TODO: delete text output, only json ?
+# TODO: delete text output, only json ? (remove pretty_print counters as well)
 if __name__=="__main__":
 
     parser=argparse.ArgumentParser(description=__doc__, 
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-p', '--path', type=str, required=True, 
                         help='Directory containing embedding.json files.')
-    parser.add_argument("-a", "-aggregation", 
-                        type=str, 
-                        choices=["mean", "median", "max", "none"], 
-                        default="mean", 
-                        help="Type of embedding aggregation.")
     parser.add_argument("-s", "--search", 
                         type=str, 
-                        choices=["dot", "nn"],
+                        choices=["dot", "nn"], 
                         default="dot", 
                         help="Type of similarity search algorithm.")
     parser.add_argument('-N', 
                         type=int, 
                         default=205, 
                         help="Number of queries to return.")
-    parser.add_argument("--no-normalization",
-                        action="store_false", 
-                        help="Do not normalize the aggregated clip embedding.")
     args=parser.parse_args()
 
     # Read all the json files in the tree
     embed_paths = glob.glob(os.path.join(args.path, "**", "*.json"), recursive=True)
     print(f"{len(embed_paths)} embeddings were found in the directory.")
 
-    # Load the embeddings and process them
-    print("Reading the embeddings and pre-processing them...")
-    start_time = time.time()
+    # Load the embeddings
     embeddings, audio_paths, str_len = [], [], 0
     for embed_path in embed_paths:
-        # Load the json file
         with open(embed_path, 'r') as infile:
-            model_outputs = json.load(infile)
-        # Process and collect
-        if model_outputs['embeddings'] is not None: # Filter out the None types
-            clip_embedding = aggregate_frames(model_outputs["embeddings"], 
-                                              normalize=args.no_normalization, 
-                                              aggregation=args.a)
-            embeddings.append(clip_embedding)
-            audio_paths.append(model_outputs["audio_path"])
-            # For pretty printing
-            if len(model_outputs["audio_path"]) > str_len:
-                str_len = len(model_outputs["audio_path"])
+            clip_embedding = json.load(infile)
+        embeddings.append(np.array(clip_embedding["embeddings"]))
+        audio_paths.append(clip_embedding["audio_path"])
+        # For pretty print
+        if len(clip_embedding["audio_path"]) > str_len:
+            str_len = len(clip_embedding["audio_path"])
     print(f"{len(embeddings)} embeddings were read.")
-    total_time = time.time()-start_time
-    print(f"Total pre-processing time: {time.strftime('%H:%M:%S', time.gmtime(total_time))}")
 
     # Perform the sound search
-    print("\nFor each sound, searching for similar sounds...")
+    print("For each sound, searching for similar sounds...")
     start_time = time.time()
     similarity_scores, similarity_indices = [], []
     for i,query in enumerate(embeddings):
@@ -127,8 +86,8 @@ if __name__=="__main__":
     model_name = os.path.basename(args.path)
     dataset_name = os.path.basename(os.path.dirname(args.path))
     export_dir = os.path.join(ANALYSIS_DIR, dataset_name, model_name)
-    output_path = os.path.join(export_dir, f"{args.search}-{args.a}-results.json")
-    print(f"\nExporting analysis results to: {output_path}")
+    output_path = os.path.join(export_dir, f"{args.search}-results.json")
+    print(f"Exporting analysis results to: {output_path}")
     os.makedirs(export_dir, exist_ok=True)
 
     # Export results to a json file
