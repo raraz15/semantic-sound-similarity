14312b4c23cf62b354ac2ce940923e1a386c670d
raraz15 <oguza97@gmail.com>
2023-03-02 21:11:16 +0100

    6 pages

--- similarity_search.py
+++ similarity_search.py
@@ -48,7 +48,7 @@ if __name__=="__main__":
                         help="Type of similarity search algorithm.")
     parser.add_argument('-N', 
                         type=int, 
-                        default=45, 
+                        default=90, 
                         help="Number of queries to return.")
     args=parser.parse_args()
 
