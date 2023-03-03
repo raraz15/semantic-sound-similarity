ba41e1dc738fa838a3fd2371c3b62ef42b7b0225
raraz15 <oguza97@gmail.com>
2023-03-03 16:22:51 +0100

    experiment variable writing

--- pipeline_experiment_scripts/essentia-prepare_search_evaluate_experiments.sh
+++ pipeline_experiment_scripts/essentia-prepare_search_evaluate_experiments.sh
@@ -5,9 +5,13 @@ export PATH="$SCRIPT_DIR:$PATH"
 
 #############################################################################
 
-essentia-prepare_search_evaluate.sh 100
-essentia-prepare_search_evaluate.sh 100
-essentia-prepare_search_evaluate.sh 200
-essentia-prepare_search_evaluate.sh -1
+# Define here the variables of the experiment.
+variable=(100 200 -1)
+
+#############################################################################
+
+for v in ${variable[@]}; do
+  essentia-prepare_search_evaluate.sh $v
+done
 
 #############################################################################
--- pipeline_experiment_scripts/yamnet-prepare_search_evaluate_experiments.sh
+++ pipeline_experiment_scripts/yamnet-prepare_search_evaluate_experiments.sh
@@ -5,16 +5,22 @@ export PATH="$SCRIPT_DIR:$PATH"
 
 #############################################################################
 
-yamnet-prepare_search_evaluate.sh "mean" 100 "" "dot"
-yamnet-prepare_search_evaluate.sh "mean" 100 "--no-normalization" "dot"
-yamnet-prepare_search_evaluate.sh "mean" 200 "" "dot"
-yamnet-prepare_search_evaluate.sh "mean" 200 "--no-normalization" "dot"
-yamnet-prepare_search_evaluate.sh "mean" -1 "" "dot"
-yamnet-prepare_search_evaluate.sh "mean" -1 "--no-normalization" "dot"
+# Define here the variables of the experiment
+variable1=("mean")
+variable2=(100 200 -1)
+variable3=("--no-normalization" "")
+variable4=("dot" "nn")
 
-yamnet-prepare_search_evaluate "mean" 100 "" "nn"
-yamnet-prepare_search_evaluate "mean" 100 "--no-normalization" "nn"
-yamnet-prepare_search_evaluate "mean" 200 "" "nn"
-yamnet-prepare_search_evaluate "mean" 200 "--no-normalization" "nn"
-yamnet-prepare_search_evaluate "mean" -1 "" "nn"
-yamnet-prepare_search_evaluate "mean" -1 "--no-normalization" "nn"
+#############################################################################
+
+for v1 in ${variable1[@]}; do
+    for v2 in ${variable2[@]}; do
+        for idx in ${!variable3[@]}; do
+            v3=${variable3[$idx]} # To deal with the "" string
+            for v4 in ${variable4[@]}; do
+                echo "$v1 $v2 $v3 $v4"
+                yamnet-prepare_search_evaluate.sh $v1 $v2 $v3 $v4
+            done
+        done
+    done
+done
