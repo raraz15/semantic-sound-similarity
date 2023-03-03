61f1e8988a1cd4a9e2156657fc7601209a659ce1
raraz15 <oguza97@gmail.com>
2023-03-03 11:47:27 +0100

    essentia_search_and_evaluate

---
+++ bash_scripts/essentia_search_and_evaluate.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Takes prepared embeddings, searches for similarity, and performs the evalu-
+# ation pipeline.
+# $1 = Prepared embedding suffix
+# $2 = search type
+
+source ~/.bashrc
+source ps/bin/activate
+
+#############################################################################
+
+DATA_DIR="/home/roguz/freesound/freesound-perceptual_similarity/data"
+DATASET_NAME="eval"
+MODEL_NAME="fs-essentia-extractor_legacy"
+
+#############################################################################
+
+EMBED_DIR="$DATA_DIR/embeddings/$DATASET_NAME"
+SIMILARITY_DIR="$DATA_DIR/similarity_results/$DATASET_NAME/$MODEL_NAME"
+EVAL_DIR="$DATA_DIR/evaluation_results/$DATASET_NAME/$MODEL_NAME"
+PREP_EMBED_DIR="$EMBED_DIR/$MODEL_NAME-$1"
+
+#############################################################################
+
+echo "======================================================================="
+echo "Working with:"
+echo $PREP_EMBED_DIR
+echo $SIMILARITY_DIR
+echo $EVAL_DIR
+echo
+
+#############################################################################
+
+# Perform similarity search
+echo "======================================================================="
+echo "Similarity Search"
+python similarity_search.py -p=$PREP_EMBED_DIR -s=$2
+SIMILARITY_PATH="$SIMILARITY_DIR-$1/$2/similarity_results.json"
+echo $SIMILARITY_PATH
+echo
+
+#############################################################################
+
+# Evaluate
+echo "======================================================================="
+echo "Evaluation"
+python evaluate.py -p=$SIMILARITY_PATH
+echo
+echo "======================================================================="
+
+#############################################################################
---
+++ bash_scripts/essentia_search_and_evaluate_experiments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR="/home/roguz/freesound/freesound-perceptual_similarity/bash_scripts/"
+export PATH="$SCRIPT_DIR:$PATH"
+
+#############################################################################
+
+essentia_search_and_evaluate.sh "PCA_100" "nn"
+essentia_search_and_evaluate.sh "PCA_200" "nn"
+essentia_search_and_evaluate.sh "PCA_1024" "nn"
