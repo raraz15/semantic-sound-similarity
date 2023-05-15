f2400f90221ee6553e961a18c7ed06893a63deef
raraz15 <oguza97@gmail.com>
2023-05-15 15:36:36 +0200

    Subset Based

--- utilities/analysis_listener.py
+++ utilities/analysis_listener.py
@@ -23,39 +23,47 @@ SAMPLE_RATE = 16000
 
 st.set_page_config(page_title="Sound Similarity", page_icon=":loud_sound:", layout="wide")
 
-def sample_sound(df, similarity_dict, sound_names, N=15):
+def display_query_and_similar_sound(fname, df, similarity_dict, N=15, header=None):
 
-    idx = random.randint(0,len(similarity_dict))
-    fname = sound_names[idx]
+    query_path = os.path.join(AUDIO_DIR, f"{fname}.wav")
     labels =  df[df.fname==int(fname)].labels.values[0]
-    similarities = similarity_dict[fname]
-    audio_path = os.path.join(AUDIO_DIR, f"{fname}.wav")
+    if header is not None:
+        st.header(header)
     with st.container():
         st.subheader("Query Sound")
         st.caption(f"Sound ID: {fname}")
         st.caption(f"Labels: {labels}")
-        audio = MonoLoader(filename=audio_path,
+        audio = MonoLoader(filename=query_path,
                             sampleRate=SAMPLE_RATE)()
         st.audio(audio, sample_rate=SAMPLE_RATE)
     st.divider()
     st.subheader(f"Top {N} Similar Sounds")
     with st.container():
         columns = st.columns(3)
-        for i, result in enumerate(similarities[:N]):
+        for i, result in enumerate(similarity_dict[fname][:N]):
             fname = list(result.keys())[0]
             labels = df[df.fname==int(fname)].labels.values[0]
-            #if len(labels) < max_length:
-            #    labels += " "*(max_length-len(labels))
             audio_path = os.path.join(AUDIO_DIR, f"{fname}.wav")
             audio = MonoLoader(filename=audio_path,
                                 sampleRate=SAMPLE_RATE)()
             with columns[i//5]:
                 st.write(f"Ranking: {i+1}")
-                #st.caption(f"Sound ID: {fname}")
                 st.caption(f"Labels: {labels}")
                 st.audio(audio, sample_rate=SAMPLE_RATE)
-                #st.divider()
-    return similarities
+
+def sample_sound(df, similarity_dict, fnames, N=15):
+
+    idx = random.randint(0,len(similarity_dict))
+    fname = fnames[idx]
+    display_query_and_similar_sound(fname, df, similarity_dict, N=N)
+
+def get_subset(sound_class, df, similarity_dict, N=15):
+
+    fnames_of_class = df[df.labels.str.contains(sound_class)].fname.to_list()
+    idx = random.randint(0,len(fnames_of_class))
+    fname = str(fnames_of_class[idx])
+    header = f"Random Sound Containing '{sound_class}' Label"
+    display_query_and_similar_sound(fname, df, similarity_dict, N=N, header=header)
 
 if __name__=="__main__":
 
@@ -77,22 +85,31 @@ if __name__=="__main__":
     # Load the analysis file
     with open(args.path ,"r") as infile:
         similarity_dict = json.load(infile)
-    sound_names = list(similarity_dict.keys())
+    fnames = list(similarity_dict.keys())
 
     # Read the meta data
     df = pd.read_csv(GT_PATH)
     df.labels = df.labels.apply(lambda x: re.sub("_", " ", re.sub(",", ", ", x)))
-    #max_length = max([len(x) for x in df.labels.to_list()])
+    all_labels = sorted(list(set([y for x in df.labels.to_list() for y in x.split(", ")])))
 
     st.title("Freesound Sound Similarity Results")
     st.text(f"Embeddings: {embeddings}")
     st.text(f"Search Method: {search}")
     st.text(f"Dataset: FSD50K.eval_audio")
-    st.header("Click To Sample a Random Sound and Display Similar Sounds.")
 
+    st.header("Click Below To Sample a Random Sound.")
     st.button(label=":loud_sound:", 
             key="sample", 
             on_click=sample_sound, 
-            args=(df, similarity_dict, sound_names), 
+            args=(df, similarity_dict, fnames), 
+            kwargs={"N":args.N}
+            )
+
+    st.header("Choose a Sound Category and Click.")
+    sound_class = st.selectbox("Sound Classes", options=all_labels)
+    print(type(sound_class))
+    st.button(label=":sound:", 
+            on_click=get_subset, 
+            args=(sound_class, df, similarity_dict), 
             kwargs={"N":args.N}
             )
