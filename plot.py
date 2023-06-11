546649b280045d70238b2950f1184c24c4d705fa
raraz15 <oguza97@gmail.com>
2023-06-11 17:51:05 +0200

    fix plot names, more formatting

--- plot.py
+++ plot.py
@@ -1,7 +1,8 @@
 import os
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
-from plotting import plot_map_at_15, plot_label_based_p_at_15, plot_av_label_based_p_at_15, plot_mr1
+from plotting import (plot_micro_map_at_15_comparisons, plot_label_based_map_at_15, 
+                        plot_macro_map_at_15_comparisons, plot_mr1)
 from directories import FIGURES_DIR, EVAL_DIR
 
 if __name__=="__main__":
@@ -22,11 +23,13 @@ if __name__=="__main__":
                         help="Dataset used for evaluating.")
     args=parser.parse_args()
 
+    # Create the save directory if it does not exist
     if args.save_dir == "":
         args.save_dir = os.path.join(FIGURES_DIR, args.model)
 
-    plot_map_at_15(args.model, EVAL_DIR, args.dataset, save_fig=True, save_dir=args.save_dir)
-    plot_label_based_p_at_15(args.model, EVAL_DIR, args.dataset, save_fig=True, save_dir=args.save_dir)
-    plot_av_label_based_p_at_15(args.model, EVAL_DIR, args.dataset, save_fig=True, save_dir=args.save_dir)
+    # Plot the figures
+    plot_micro_map_at_15_comparisons(args.model, EVAL_DIR, args.dataset, save_fig=True, save_dir=args.save_dir)
+    plot_label_based_map_at_15(args.model, EVAL_DIR, args.dataset, save_fig=True, save_dir=args.save_dir)
+    plot_macro_map_at_15_comparisons(args.model, EVAL_DIR, args.dataset, save_fig=True, save_dir=args.save_dir)
     plot_mr1(args.model, EVAL_DIR, args.dataset, save_fig=True, save_dir=args.save_dir)
     print("Done!")
--- plotting/single_model_plots.py
+++ plotting/single_model_plots.py
@@ -12,7 +12,89 @@ from matplotlib.colors import TABLEAU_COLORS
 COLORS = list(TABLEAU_COLORS.values())
 
 ###################################################################################################
-# Micro map#k
+# Micro-averaged map@k
+
+def plot_micro_map_at_15_comparisons(model, eval_dir, dataset_name, fig_name="", D=0.25, save_fig=False, save_dir=""):
+    """Takes a model name and plots the micro-averaged mAP@k for all the variations of the model."""
+
+    # Find all the variation_paths of the model
+    variation_paths = sorted(glob.glob(os.path.join(eval_dir, dataset_name, f"{model}-*")))
+    # Read one variation's folder to get the searches
+    searches = os.listdir(variation_paths[0])
+    # Read one map to get the k values
+    map_path = os.path.join(variation_paths[0], searches[0], "micro_mAP.csv")
+
+    # Read all the maps
+    map_dict = {search: [] for search in searches}
+    for search in searches:
+        for model_dir in variation_paths:
+            map_path = os.path.join(model_dir, search, "micro_mAP.csv")
+            map = pd.read_csv(map_path)
+            full_model_name = model_dir.split("/")[-1]
+            variation = "-".join(full_model_name.split("-")[-3:])
+            map_dict[search].append((variation, map[map.k==15].mAP.to_numpy()[0]))
+
+    # Determine some plot parameters
+    if len(searches)>1:
+        positions = np.linspace(-D, D, len(searches))
+        delta = positions[1]-positions[0]
+    else:
+        positions = [0]
+        delta = 1
+    fig_name = fig_name if fig_name else f"Embedding Processing and Search Algorithm"+ \
+                f"Performances by mAP Values (Micro-Averaged) \n{model} Evaluated on {dataset_name}"
+
+    # Plot the maps
+    fig, ax = plt.subplots(figsize=(18,6), constrained_layout=True)
+    fig.suptitle(fig_name, fontsize=19, weight='bold')
+    xticks = []
+    for j,search in enumerate(map_dict.keys()):
+        for z,(variation,map) in enumerate(map_dict[search]):
+            if j==0:
+                if "essentia" not in variation:
+                    xticks.append(variation.replace("-","\n").replace("Agg_", ""))
+                else:
+                    xticks.append(variation.replace("essentia-extractor_legacy-", ""))
+            if z==0:
+                if search=="dot":
+                    label = "Dot Product"
+                elif search=="nn":
+                    label = "Nearest Neighbors"
+            else:
+                label = ""
+            ax.bar(z+positions[j], 
+                            height=map, 
+                            width=delta*0.6, 
+                            label=label, 
+                            color=COLORS[j], 
+                            edgecolor='k')
+            ax.text(z+positions[j], 
+                            map+0.01, 
+                            f"{map:.3f}", 
+                            ha='center', 
+                            va='bottom', 
+                            fontsize=10)
+
+    ax.set_title(f"Page 1 Results", fontsize=17)
+    ax.tick_params(axis='y', which='major', labelsize=11)
+    ax.tick_params(axis='x', which='major', labelsize=10)
+    ax.set_xticks(np.arange(len(xticks)), xticks)
+    ax.set_yticks(np.arange(0,1.05,0.05))
+    ax.grid()
+    ax.set_ylabel("mAP@15 (↑)", fontsize=15)
+    ax.set_xlabel("Processing Parameters", fontsize=15)
+    ax.legend(fontsize=10, loc=1, title="Search Algorithms", 
+                        title_fontsize=10, fancybox=True)
+    ax.set_ylim([0,1])
+    if save_fig:
+        if save_dir == "":
+            print("Please provide a save directory if you want to save the figure.")
+            sys.exit(1)
+        os.makedirs(save_dir, exist_ok=True)
+        fig_path = os.path.join(save_dir, f"{model}-micro_mAP@15-comparisons.png")
+        print(f"Saving figure to {fig_path}")
+        fig.savefig(fig_path)
+    plt.show()
 
 def plot_map_at_all_k(model, eval_dir, dataset_name, fig_name="", n_cols=3, save_fig=False, save_dir=""):
     """Takes a model name and plots the mAP@k for all the variations of the model."""
@@ -101,97 +183,14 @@ def plot_map_at_all_k(model, eval_dir, dataset_name, fig_name="", n_cols=3, save
         fig.savefig(fig_path)
     plt.show()
 
-def plot_map_at_15(model, eval_dir, dataset_name, fig_name="", D=0.25, save_fig=False, save_dir=""):
-    """Takes a model name and plots the mAP@k for all the variations of the model."""
-
-    # Find all the variation_paths of the model
-    variation_paths = sorted(glob.glob(os.path.join(eval_dir, dataset_name, f"{model}-*")))
-    # Read one variation's folder to get the searches
-    searches = os.listdir(variation_paths[0])
-    # Read one map to get the k values
-    map_path = os.path.join(variation_paths[0], searches[0], "mAP.csv")
-
-    # Read all the maps
-    map_dict = {search: [] for search in searches}
-    for search in searches:
-        for model_dir in variation_paths:
-            map_path = os.path.join(model_dir, search, "mAP.csv")
-            map = pd.read_csv(map_path)
-            full_model_name = model_dir.split("/")[-1]
-            variation = "-".join(full_model_name.split("-")[-3:])
-            map_dict[search].append((variation, map[map.k==15].mAP.to_numpy()[0]))
-
-    # Determine some plot parameters
-    if len(searches)>1:
-        positions = np.linspace(-D, D, len(searches))
-        delta = positions[1]-positions[0]
-    else:
-        positions = [0]
-        delta = 1
-    fig_name = fig_name if fig_name else f"Embedding Processing and Search Algorithm \
-                Performances by mAP Values \n{model} Evaluated on {dataset_name}"
-
-    # Plot the maps
-    fig, ax = plt.subplots(figsize=(18,6), constrained_layout=True)
-    fig.suptitle(fig_name, fontsize=19, weight='bold')
-    xticks = []
-    for j,search in enumerate(map_dict.keys()):
-        for z,(variation,map) in enumerate(map_dict[search]):
-            if j==0:
-                if "essentia" not in variation:
-                    xticks.append(variation.replace("-","\n").replace("Agg_", ""))
-                else:
-                    xticks.append(variation.replace("essentia-extractor_legacy-", ""))
-            if z==0:
-                if search=="dot":
-                    label = "Dot Product"
-                elif search=="nn":
-                    label = "Nearest Neighbors"
-            else:
-                label = ""
-            ax.bar(z+positions[j], 
-                            height=map, 
-                            width=delta*0.6, 
-                            label=label, 
-                            color=COLORS[j], 
-                            edgecolor='k')
-            ax.text(z+positions[j], 
-                            map+0.01, 
-                            f"{map:.3f}", 
-                            ha='center', 
-                            va='bottom', 
-                            fontsize=10)
-
-    ax.set_title(f"Page 1 Results", fontsize=17)
-    ax.tick_params(axis='y', which='major', labelsize=11)
-    ax.tick_params(axis='x', which='major', labelsize=10)
-    ax.set_xticks(np.arange(len(xticks)), xticks)
-    ax.set_yticks(np.arange(0,1.05,0.05))
-    ax.grid()
-    ax.set_ylabel("mAP@15 (↑)", fontsize=15)
-    ax.set_xlabel("Processing Parameters", fontsize=15)
-    ax.legend(fontsize=10, loc=1, title="Search Algorithms", 
-                        title_fontsize=10, fancybox=True)
-    ax.set_ylim([0,1])
-    if save_fig:
-        if save_dir == "":
-            print("Please provide a save directory if you want to save the figure.")
-            sys.exit(1)
-        os.makedirs(save_dir, exist_ok=True)
-        fig_path = os.path.join(save_dir, f"{model}-mAP@15.png")
-        print(f"Saving figure to {fig_path}")
-        fig.savefig(fig_path)
-    plt.show()
-
 ####################################################################################################
-# Macro mAP@k
+# Macro-averaged mAP@k
 
-def plot_av_label_based_p_at_15(model, eval_dir, dataset_name, fig_name="", D=0.25, save_fig=False, save_dir=""):
-    """ Takes a model name and plots the label-based Precision@15 (Macro Averaged Precision@15) 
-    for all the variations."""
+def plot_macro_map_at_15_comparisons(model, eval_dir, dataset_name, fig_name="", D=0.25, save_fig=False, save_dir=""):
+    """Takes a model name and for each variation, plots the Macro-averaged mAP@15."""
 
-    default_fig_name = f"Embedding Processing and Search Algorithm Performances by Average Label-Based Precision@15\
-        \n{model} Evaluated on {dataset_name}"
+    default_fig_name = f"Embedding Processing and Search Algorithm Performances by "+\
+                f"Label-Based mAP@15\n{model} Evaluated on {dataset_name}"
 
     # Find all the variation_paths of the model
     variation_paths = sorted(glob.glob(os.path.join(eval_dir, dataset_name, f"{model}-*")))
@@ -202,9 +201,8 @@ def plot_av_label_based_p_at_15(model, eval_dir, dataset_name, fig_name="", D=0.
     map_dict = {search: [] for search in searches}
     for search in searches:
         for variation_path in variation_paths:
-            map_path = os.path.join(variation_path, search, "macro_averaged_precision_at_15.txt")
-            with open(map_path, "r") as f:
-                map = float(f.read())
+            map_path = os.path.join(variation_path, search, "macro_map@15.csv")
+            map = pd.read_csv(map_path)["macro_map@15"]
             full_model_name = variation_path.split("/")[-1]
             variation = "-".join(full_model_name.split("-")[-3:])
             map_dict[search].append((variation, map))
@@ -265,16 +263,16 @@ def plot_av_label_based_p_at_15(model, eval_dir, dataset_name, fig_name="", D=0.
             print("Please provide a save directory if you want to save the figure.")
             sys.exit(1)
         os.makedirs(save_dir, exist_ok=True)
-        fig_path = os.path.join(save_dir, f"{model}-label_based_prec@15_comparisons.png")
+        fig_path = os.path.join(save_dir, f"{model}-macro_map@15-comparisons.png")
         print(f"Saving figure to {fig_path}")
         fig.savefig(fig_path)
     plt.show()
 
-def plot_label_based_p_at_15(model, eval_dir, dataset_name, fig_name="", save_fig=False, save_dir=""):
+def plot_label_based_map_at_15(model, eval_dir, dataset_name, fig_name="", save_fig=False, save_dir=""):
     """Takes an embedding name and find all the variations of the model. For each variation,
-    plots the label-based Precision@15 (Macro Averaged Precision@15)."""
+    plots the Average Precision@15 of all labels."""
 
-    default_fig_name = f"Label-Based Precision@15 for {model} Evaluated on {dataset_name}"
+    default_fig_name = f"Label-Based mAP@15 Values for {model} Evaluated on {dataset_name}"
 
     # Find all the variation_paths of the model
     variation_paths = sorted(glob.glob(os.path.join(eval_dir, dataset_name, f"{model}-*")))
@@ -286,41 +284,39 @@ def plot_label_based_p_at_15(model, eval_dir, dataset_name, fig_name="", save_fi
         for search in searches:
 
             # Read the label-based mAP@15
-            in_path = os.path.join(variation_path, search, "label_positive_rates.csv")
-            label_rates = pd.read_csv(in_path)
+            in_path = os.path.join(variation_path, search, "labels_mAP@15.csv")
+            labels_map = pd.read_csv(in_path)
 
-            # Calculate the Precision@15 for each label
-            label_rates["precision"] = label_rates["tp"] / (label_rates["tp"] + label_rates["fp"])
-            # Separate the labels and maps
-            labels = label_rates["label"].to_list()
-            precisions = label_rates["precision"].to_list()
+            # Get the labels and maps
+            labels = labels_map["label"].to_list()
+            maps = labels_map["map@15"].to_list()
             # Sort the labels and maps
-            label_precisions = sorted(zip(labels, precisions), key=lambda x: x[1], reverse=True)
+            label_aps = sorted(zip(labels, maps), key=lambda x: x[1], reverse=True)
 
             # Determine some plot parameters
             N = 10 # Number of rows
-            delta = len(precisions) // N
+            delta = len(maps) // N
             embedding_search = os.path.basename(variation_path) + "-" + search
             fig_name = fig_name if fig_name else default_fig_name
 
-            # Plot the label-based Precision@15
+            # Plot the label-based mAP@15
             fig, ax = plt.subplots(figsize=(18, 24), nrows=N, constrained_layout=True)
             fig.suptitle(fig_name, fontsize=16)
             for i in range(N):
                 ax[i].bar(
-                        [label.replace("_","\n") for label,_ in label_precisions[i*delta:(i+1)*delta]], 
-                        [prec for _,prec in label_precisions[i*delta:(i+1)*delta]]
+                        [label.replace("_","\n") for label,_ in label_aps[i*delta:(i+1)*delta]], 
+                        [prec for _,prec in label_aps[i*delta:(i+1)*delta]]
                         )
                 ax[i].set_yticks(np.arange(0, 1.05, 0.2))
                 ax[i].grid()
                 ax[i].set_ylim([0, 1.05])
-                ax[i].set_ylabel("Precision@15")
+                ax[i].set_ylabel("mAP@15")
             if save_fig:
                 if save_dir == "":
                     print("Please provide a save directory if you want to save the figure.")
                     sys.exit(1)
                 os.makedirs(save_dir, exist_ok=True)
-                fig_path = os.path.join(save_dir, f"{embedding_search}-label_based_prec@15.png")
+                fig_path = os.path.join(save_dir, f"{embedding_search}-label_based_mAP@15.png")
                 print(f"Saving figure to {fig_path}")
                 fig.savefig(fig_path)
                 plt.close()
