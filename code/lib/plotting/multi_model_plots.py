f5fe6d7f9079845a85b7e256f0aedebf38aea58e
raraz15 <oguza97@gmail.com>
2023-07-07 15:51:10 +0200

    single method for micro macro map plots

--- code/lib/plotting/multi_model_plots.py
+++ code/lib/plotting/multi_model_plots.py
@@ -17,7 +17,7 @@ from ..directories import EVAL_DIR
 
 DATASET_NAME = "FSD50K.eval_audio"
 
-# TODO: how to encode variation and the search?
+###################################################################################
 
 def _save_function(save_fig, save_dir, default_name, fig, models):
 
@@ -33,13 +33,32 @@ def _save_function(save_fig, save_dir, default_name, fig, models):
             for model in models:
                 infile.write(f"{model[0]}-{model[1]}\n")
 
-####################################################################################################
+###################################################################################
 # mAP
 
-def plot_micro_map_comparisons_multimodel(models, eval_dir=EVAL_DIR, dataset_name=DATASET_NAME, fig_name="", save_fig=False, save_dir=""):
-    """Takes a list of [(model,variation,search)] and plots all the Micro Averaged mAP@k in the same figure."""
+# TODO: how to encode variation and the search?
+def plot_map_comparisons_multimodel(models, map_type, 
+                                    eval_dir=EVAL_DIR, dataset_name=DATASET_NAME, 
+                                    fig_name="", save_fig=False, save_dir=""):
+    """Takes a list of [(model,variation,search)] and plots all the mAP@15 in 
+    the same figure. By setting map_type to "micro" or "macro" you can choose 
+    between the two types of mAP.
+    """
 
-    default_fig_name = f"Embedding Performances using Instance-Based mAP@15 (Micro-Averaged) on {dataset_name}"
+    # Determine the file name and figure name
+    if map_type=="micro":
+        file_name = "micro_mAP@15.txt"
+        default_fig_name = "Sound Similarity Performances of Embeddings using "\
+                        f"Instance-Based mAP@15 on {dataset_name}" #(Micro-Averaged)
+        figure_save_name = "best_embeddings-micro_mAP@15-comparison.png"
+    elif map_type=="macro":
+        file_name = "balanced_mAP@15.txt"
+        default_fig_name = "Sound Similarity Performances of Embeddings using "\
+                        f"Label-Based mAP@15 on {dataset_name}" # (Macro-Averaged)
+        figure_save_name = "best_embeddings-macro_mAP@15-comparison.png"
+    else:
+        raise("map_type must be one of 'micro', 'macro'")
+    fig_name = fig_name if fig_name else default_fig_name
 
     # Determine Some Parameters
     positions = np.linspace(-0.4, 0.4, len(models))
@@ -48,96 +67,45 @@ def plot_micro_map_comparisons_multimodel(models, eval_dir=EVAL_DIR, dataset_nam
     # Read the mAP for each model
     maps = []
     for model, variation, search in models:
-        map_path = os.path.join(eval_dir, dataset_name, model+"-"+variation, search, "micro_mAP@15.txt")
+        embedding_eval_dir = os.path.join(eval_dir, dataset_name, model+"-"+variation)
+        map_path = os.path.join(embedding_eval_dir, search, file_name)
         with open(map_path, "r") as in_f:
-            micro_map_at_15 = float(in_f.read())
-        maps.append((model, variation, search, micro_map_at_15))
+            map_at_15 = float(in_f.read())
+        maps.append((model, variation, search, map_at_15))
 
-    # Plot the micro-mAP for each model
+    # Plot the mAP for each model in a single figure
     fig,ax = plt.subplots(figsize=(18,6), constrained_layout=True)
-    fig_name = fig_name if fig_name else default_fig_name
     fig.suptitle(fig_name, fontsize=19, weight='bold')
-    ax.set_title("Page 1 Results", fontsize=15)
-    for j,(model,variation,search,micro_map_at_15) in enumerate(maps):
+    for j,(model,variation,search,map_at_15) in enumerate(maps):
         ax.bar(positions[j], 
-                micro_map_at_15, 
+                map_at_15, 
                 label=model,
                 width=delta*0.80, 
                 color=COLORS[j], 
-                edgecolor='k'
-                )
+                edgecolor='k',
+                linewidth=1.6)
         ax.text(positions[j], 
-                micro_map_at_15+0.01, 
-                f"{micro_map_at_15:.3f}", 
+                map_at_15+0.01, 
+                f"{map_at_15:.3f}", 
                 ha='center', 
                 va='bottom', 
-                fontsize=10, 
-                weight='bold'
-                )
+                fontsize=12, 
+                weight='bold')
 
     # Set the plot parameters
+    # ax.set_title("Page 1 Results", fontsize=15)
     ax.set_yticks(np.arange(0,1.05,0.05))
     ax.tick_params(axis='x', which='major', labelsize=0)
     ax.tick_params(axis='y', which='major', labelsize=11)
-    ax.set_xlabel("Embedding, Search Combinations", fontsize=15)
+    ax.set_xlabel("Embeddings", fontsize=15) # , Search Combinations
     ax.set_ylabel("mAP@15 (↑)", fontsize=15) # TODO: change name?
     ax.set_ylim([0,1])
     ax.grid()
-    ax.legend(loc="best", fontsize=10, title_fontsize=11, fancybox=True)
+    ax.legend(loc="best", fontsize=11, title="Embeddings", 
+              title_fontsize=11, fancybox=True)
 
     # Save and show
-    _save_function(save_fig, save_dir, "best_embeddings-micro_mAP@15-comparison.png", fig, models)
-    plt.show()
-
-def plot_macro_map_comparisons_multimodel(models, eval_dir=EVAL_DIR, dataset_name=DATASET_NAME, fig_name="", save_fig=False, save_dir=""):
-    """Takes a list of [(model,variation,search)] and plots all the Macro Averaged mAP@15 in the same figure."""
-
-    default_fig_name = f"Embedding Performances using Label-Based mAP@15 (Macro-Averaged) on {dataset_name}"
-
-    # Determine Some Parameters
-    positions = np.linspace(-0.4, 0.4, len(models))
-    delta = positions[1]-positions[0]
-
-    # Read the mAP for each model
-    maps = []
-    for model, variation, search in models:
-        map_path = os.path.join(eval_dir, dataset_name, model+"-"+variation, search, "balanced_mAP@15.txt")
-        with open(map_path, "r") as in_f:
-            balanced_map_at_15 = float(in_f.read())
-        maps.append((model, variation, search, balanced_map_at_15))
-
-    fig,ax = plt.subplots(figsize=(18,6), constrained_layout=True)
-    fig_name = fig_name if fig_name else default_fig_name
-    fig.suptitle(fig_name, fontsize=19, weight='bold')
-    ax.set_title("Page 1 Results", fontsize=15)
-    for j,(model,variation,search,balanced_map_at_15) in enumerate(maps):
-        ax.bar(positions[j], 
-                balanced_map_at_15, 
-                label=model,
-                width=delta*0.80, 
-                color=COLORS[j], 
-                edgecolor='k'
-                )
-        ax.text(positions[j], 
-                balanced_map_at_15+0.01, 
-                f"{balanced_map_at_15:.3f}", 
-                ha='center', 
-                va='bottom', 
-                fontsize=10, 
-                weight='bold'
-                )
-
-    # Set the plot parameters
-    ax.set_yticks(np.arange(0,1.05,0.05))
-    ax.tick_params(axis='x', which='major', labelsize=0)
-    ax.tick_params(axis='y', which='major', labelsize=11)
-    ax.set_xlabel("Embedding, Search Combinations", fontsize=15)
-    ax.set_ylabel("mAP@15 (↑)", fontsize=15) # TODO: change name?
-    ax.set_ylim([0,1])
-    ax.grid()
-    ax.legend(loc="best", fontsize=10, title_fontsize=11, fancybox=True)
-
-    _save_function(save_fig, save_dir, "best_embeddings-macro_mAP@15-comparison.png", fig, models)
+    _save_function(save_fig, save_dir, figure_save_name, fig, models)
     plt.show()
 
 def plot_family_map_comparisons_multimodel(models, eval_dir=EVAL_DIR, dataset_name=DATASET_NAME, fig_name="", save_fig=False, save_dir=""):
@@ -148,7 +116,8 @@ def plot_family_map_comparisons_multimodel(models, eval_dir=EVAL_DIR, dataset_na
     # Read the mAP for each model
     model_maps = defaultdict(list)
     for model, variation, search in models:
-        map_path = os.path.join(eval_dir, dataset_name, model+"-"+variation, search, "families_mAP@15.csv")
+        embedding_eval_dir = os.path.join(eval_dir, dataset_name, model+"-"+variation)
+        map_path = os.path.join(embedding_eval_dir, search, "families_mAP@15.csv")
         labels_map = pd.read_csv(map_path)
         families = labels_map["family"].to_list()
         maps = labels_map["map"].to_list()
