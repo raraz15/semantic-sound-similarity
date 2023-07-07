412682ef5ee7c069e71fb8d33695b6c4edf26a46
raraz15 <oguza97@gmail.com>
2023-07-07 10:07:52 +0200

    name change evaluate_map_at_15

--- scripts/pipelines/vggish-prepare_search_evaluate.sh
+++ scripts/pipelines/vggish-prepare_search_evaluate.sh
@@ -81,7 +81,7 @@ echo
 # Evaluate
 echo "======================================================================="
 echo "Evaluation"
-python code/evaluate.py $SIMILARITY_PATH
+python code/evaluate_map_at_15.py $SIMILARITY_PATH
 echo
 echo "======================================================================="
 
--- scripts/utilities/evaluate_all_variations_of_a_model.sh
+++ scripts/utilities/evaluate_all_variations_of_a_model.sh
@@ -18,11 +18,11 @@ for file in "$SIMILARITY_DIR/"*; do # for each similarity dir
         echo "$file"
         if [[ "$file" == *"fs-essentia-extractor_legacy-"* ]]; then
             # Run the script
-            python code/evaluate.py $file/nn/similarity_results.json #--metrics=$2
+            python code/evaluate_map_at_15.py $file/nn/similarity_results.json #--metrics=$2
         else
             # Run the script for each search type
             for search in "${searches[@]}"; do
-                python code/evaluate.py $file/$search/similarity_results.json #--metrics=$2
+                python code/evaluate_map_at_15.py $file/$search/similarity_results.json #--metrics=$2
             done
         fi
     fi
