e0b86257175ad129a2cc0328b747093ad1aab168
raraz15 <oguza97@gmail.com>
2023-08-21 10:20:29 +0200

    self supervised extractor

---
+++ code/ssl-embedding_extractor.py
@@ -0,0 +1,142 @@
+"""This script load a self-supervised model with a pre-trained checkpoint and
+extracts clip level embeddings for all the audio files in the FSD50K evaluation 
+dataset."""
+
+import os
+import time
+import json
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+
+import numpy as np
+import pandas as pd
+
+import torch
+
+from lib.directories import AUDIO_DIR, GT_PATH, EMBEDDINGS_DIR
+
+TRIM_DUR = 30 # seconds
+
+if __name__=="__main__":
+
+    parser=ArgumentParser(description=__doc__, 
+                        formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('model_path',
+                        type=str, 
+                        help="Path to model.pt chekpoint.")
+    parser.add_argument('-o', 
+                        '--output_dir', 
+                        type=str, 
+                        default="",
+                        help="Path to output directory.")
+    args=parser.parse_args()
+
+    # Get the model anem from models/model_name.pt
+    model_name = os.path.splitext(os.path.basename(args.model_path))[0]
+    # Load the corresponding model
+    if "clap" in model_name.lower():
+        from lib.laion_clap import CLAP_Module
+        # Decide type of CLAP model
+        if model_name in ["clap-630k-audioset-fusion-best", "clap-630k-fusion-best"]:
+            model = CLAP_Module(enable_fusion=True)
+        elif "clap-music_speech_audioset_epoch_15_esc_89.98" == model_name:
+            model= CLAP_Module(enable_fusion=False, amodel= 'HTSAT-base')
+        else:
+            raise ValueError(f"Unknown CLAP model name: {model_name}")
+        # Load the model
+        model.load_ckpt(args.model_path)
+        # Define embedding extractor function
+        def extract_embeddings(model, audio_path):
+            # Process
+            embeddings = model.get_audio_embedding_from_filelist(x=[audio_path], 
+                                                                    use_tensor=False).tolist()
+            return embeddings
+    elif "imagebind" in model_name.lower():
+        from lib.imagebind import data
+        from lib.imagebind.models import imagebind_model
+        from lib.imagebind.models.imagebind_model import ModalityType        
+        # Load the model
+        model = imagebind_model.imagebind_huge(args.model_path, pretrained=True)
+        model.eval()
+        model.to("cpu")
+        # Define embedding extractor function
+        def extract_embeddings(model, audio_path):
+            # Load the audio file
+            inputs = {
+                ModalityType.AUDIO: data.load_and_transform_audio_data([audio_path], 'cpu'),
+            }
+            # Process
+            with torch.no_grad():
+                embeddings = model(inputs)
+            embeddings = embeddings['audio'].cpu().numpy().tolist()
+            return embeddings
+    elif "audioclip" in model_name.lower():
+        from lib.audio_clip.model import AudioCLIP
+        from lib.audio_clip.utils.transforms import ToTensor1D
+        from essentia.standard import EasyLoader
+        model = AudioCLIP(pretrained=args.model_path).eval()
+        # Define embedding extractor function
+        # TODO: create a function to load audio files, use it in tensofrlowpredict script as well
+        def extract_embeddings(model, audio_path):
+            # Load the audio file
+            loader = EasyLoader()
+            loader.configure(filename=audio_path, 
+                            sampleRate=44100, 
+                            endTime=TRIM_DUR, # FSD50K are already below 30 seconds
+                            replayGain=0 # Do not normalize the audio
+                            )
+            audio = loader()
+            # Zero pad short clips (IN FSD50K 7% of the clips are shorter than 1 second)
+            if audio.shape[0] < 44100:
+                audio = np.concatenate((audio, np.zeros((44100-audio.shape[0]))))
+            # Bring to the right format
+            audio = audio.astype(np.float32)
+            audio_transforms = ToTensor1D()
+            audio = torch.stack([audio_transforms(audio.reshape(1,-1))])
+            # Process
+            ((embeddings, _, _), _), _ = model(audio=audio)
+            embeddings = embeddings.tolist()
+            return embeddings
+    else:
+        raise ValueError(f"Unknown model name: {model_name}.")
+
+    # Read the file names
+    fnames = pd.read_csv(GT_PATH)["fname"].to_list()
+    audio_paths = []
+    for fname in fnames:
+        audio_path = os.path.join(AUDIO_DIR, f"{fname}.wav")
+        if os.path.exists(audio_path):
+            audio_paths.append(audio_path)
+    assert len(audio_paths)==len(fnames), "Some audio files are missing."
+    print(f"There are {len(audio_paths)} audio files to process.")
+
+    # Determine the output directory
+    if args.output_dir=="":
+        # If the default output directory is used add the AUDIO_DIR to the path
+        output_dir = os.path.join(EMBEDDINGS_DIR, 
+                                os.path.basename(AUDIO_DIR),
+                                model_name)
+    else:
+        output_dir = os.path.join(args.output_dir, model_name)
+    # Create the output directory
+    os.makedirs(output_dir, exist_ok=True)
+    print(f"Exporting the embeddings to: {output_dir}")
+
+    # Process each audio clip
+    start_time = time.time()
+    for i,audio_path in enumerate(audio_paths):
+        # Extract the embeddings
+        embeddings = extract_embeddings(model, audio_path)
+        # Save results
+        fname = os.path.splitext(os.path.basename(audio_path))[0]
+        output_path = os.path.join(output_dir, f"{fname}.json")
+        with open(output_path, 'w') as outfile:
+            json.dump({'audio_path': audio_path, 'embeddings': embeddings}, outfile, indent=4)
+        # Print progress
+        if (i+1)%1000==0 or i==0 or i+1==len(audio_paths):
+            print(f"[{i+1:>{len(str(len(audio_paths)))}}/{len(audio_paths)}]")        
+    total_time = time.time()-start_time
+    print(f"\nTotal time: {time.strftime('%M:%S', time.gmtime(total_time))}")
+    print(f"Average time/file: {total_time/len(audio_paths):.2f} sec.")
+
+    #############
+    print("Done!\n")    
